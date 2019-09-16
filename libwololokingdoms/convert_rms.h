1ae2d953756f863bf1e2ac162764b7e514c06fcd
Renée Kooi <renee@kooi.me>
2019-09-16 14:36:34 +0200

    clang-format

--- libwololokingdoms/convert_rms.h
+++ libwololokingdoms/convert_rms.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <fs.h>
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 
 void convert_maps(const fs::path& input_dir, const fs::path& output_dir,
                   const std::map<std::string, fs::path>& terrain_graphics,
